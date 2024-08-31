7423fe9e4b9997266170d52111f821214c580147
IPSSandhu <ishan3121@uvic.ca>
2024-08-31 15:50:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random as rnd
 import time 
 import matplotlib.pyplot as plt
 import math
-import gym
+import gymnasium as gym
 import pygame
 
 import torch
@@ -24,12 +24,12 @@ output_dims = 2
 episodes = 0
 
 # Global Constants, change these
-MAX_EPISODES = 1
+MAX_EPISODES = 5
 
 
 if __name__ == "__main__":
     env = gym.make('CartPole-v1', render_mode='human')
-    agnet = DQNAgent(input_dims, output_dims)
+    agent = DQNAgent(input_dims, output_dims)
 
     # Make the main game loop.  
 
@@ -45,25 +45,33 @@ if __name__ == "__main__":
         # Setting the environment to the intial state at the beginnign of each episode
         observation, info = env.reset() # observation: initial state of the environment || info: any other additional info
         time_step = 0
+        total_episode_reward = 0
         done = False #flag to be used in the while loop which tells us if the episode has ended or not.
 
         # runs until the episode doesnt end/terminate/truncate 
         # Runs to process each time step of the episode
         while not done:
             
-            # Get a random action from the environment (gym), ideally through your agent
+            # Get a random action from the environment (gym), ideally through your agent,
+            # its gonna be 0 (left) or 1 (right)
             action = env.action_space.sample()
+            # action = env.action_space.get_action()
             
             # Take the action and observe the result
-            # Observation: New state of the environment after the action is taken
-            # Reward: 
+            # Observation: New state of the environment after the action is taken (cart position, cart velocity, pole angle, pole angular velocity)
+            # Reward: +1 for every step taken
             # terminated: boolean flag if the episode ended because goal was reached or agent failed
             # truncated: boolean flag indicating if ended due to time limit or other constraint
             # info: additional info by the env
+            # env.step(action) = applies action to the environment (left or right)
+           
             observation, reward, terminated, trunicated, info = env.step(action)
+            # print(env.step(1))
             
             # Accumulate the reward
-
+            total_episode_reward += reward
+            # print(total_episode_reward)
+           
             # Check if we lost
             if terminated or trunicated:
                 done = True
@@ -75,6 +83,7 @@ if __name__ == "__main__":
             #agent.learn()
             time_step += 1
 
+            # gives the visual representation of the cartpole
             env.render()
         
     # TODO: Check if reward normalization makes sense!
